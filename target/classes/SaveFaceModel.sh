d2b225cc5cb8df8f6fded307f218b78b59256cb3
yang <77215911+YangHanj@users.noreply.github.com>
2022-06-20 21:59:38 +0800

    Delete target directory

--- target/classes/SaveFaceModel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PATH=$1
-SAVE=$2
-ID=$3
-cd /Users/yanghan/Desktop/Project/project2/FaceRecognition
-/Users/yanghan/ThirdParty/conda3-4.10/anaconda3/envs/yang/bin/python LoadModel.py -path $PATH -save $SAVE -id $ID
